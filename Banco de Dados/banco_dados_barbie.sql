592493195c3caf967e1225f8640b44bfd26727ba
jordana983 <jordana.santos@sptech.school>
2023-05-22 09:53:35 -0300

    Barbie is' real - API

---
+++ Banco de Dados/banco_dados_barbie.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Barbie;
+
+USE Barbie;
+
+CREATE TABLE usuario (
+idUsuario INT PRIMARY KEY auto_increment,
+nome VARCHAR (50) NOT NULL,
+sobrenome VARCHAR (50),
+email varchar (45) NOT NULL,
+senha VARCHAR(16) NOT NULL
+);
+
+SELECT * FROM usuario;
+
+
+truncate table usuario;
